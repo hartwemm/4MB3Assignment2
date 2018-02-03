35f894fd1ee7602bb1a192b7c41419cbc187b75b
meganhartwell <32854661+meganhartwell-stemcell@users.noreply.github.com>
2018-02-02 23:36:22 -0500

    First changes to KnitR (Q1 and part of Q3)

--- Q3_Rcode.R
+++ Q3_Rcode.R
@@ -1,10 +1,14 @@
+### Part a
 library(deSolve)
 
-SIR.vector.field <- function(t, vars, parms=c(R_0=2,gamma=1)) {
+
+### Part b
+SIR.vector.field <- function(t, vars, parms=c(beta=2,gamma=1)) {
+  # Writes vector field of these differential equations
   with(as.list(c(parms, vars)), {
-    dS <- -R_0*S*I # dS/dt
-    dI <- R_0*S*I-I  # dI/dt
-    dR <- I  # dR/dt
+    dS <- -1*beta*S*I # dS/dt
+    dI <- beta*S*I-gamma*I  # dI/dt
+    dR <- gamma*I  # dR/dt
     vec.fld <- c(dS=dS, dI=dI, dR=dR)
     return(list(vec.fld)) # ode() requires a list
   })
@@ -12,14 +16,20 @@ SIR.vector.field <- function(t, vars, parms=c(R_0=2,gamma=1)) {
 
 draw.soln <- function(ic=c(S=0.999,I=0.001,R=0), tmax=1, 
                       times=seq(0,tmax,length.out = 500), 
-                      func, parms, colour="blue", ... ) {
-  soln <- ode(y=ic, times=times, func, parms)
-  lines(times, soln[,"I"], col=colour, ... )
+                      func, parms=c(R_0=2,gamma_inv=1), colour="blue", ... ) {
+  # Solve ode from vector field using deSolve package and 
+  # initial condition and parameters given
+  with(as.list(parms),{
+    gamma=1/gamma_inv
+    beta=gamma*R_0
+    # times=times/gamma_inv # Convert time to "natural units" of periods of gamma
+    soln <- ode(y=ic, times=times, func, parms=c(beta=beta,gamma=gamma)) # Solve the ode 
+    lines(times, soln[,"I"], col=colour, ... ) # Add this line to our plot
+    return(data.frame(time=times,y=soln[,"I"]))
+  })
 }
 
 
-## Part b
-## Plot solutions of the SIR model
 tmax <- 10 # end time for numerical integration of the ODE
 ## draw box for plot:
 plot(0,0,xlim=c(0,tmax),ylim=c(0,1),
@@ -28,38 +38,70 @@ plot(0,0,xlim=c(0,tmax),ylim=c(0,1),
 I0 <- 0.001
 S0 <- 1 - I0
 R0 <-  0
-## draw solutions for several values of parameter beta:
-Rknot_vals <- c(1.5,2,2.5)
-for (i in 1:length(betavals)) {
-  draw.soln(ic=c(S=S0,I=I0,R=R0), tmax=tmax,
-            func=SIR.vector.field,
-            parms=c(R_0=Rknot_vals[i],gamma=1),
-            lty=i # use a different line style for each solution
-  )
-}
-
+## Set parameter values
+Rknot_vals <- 2.5
+gamma_inv <- 1
+# Solve ode for these parameters and initial conditions
+draw.soln(ic=c(S=S0,I=I0,R=R0), tmax=tmax,
+          func=SIR.vector.field,
+          parms=c(R_0=Rknot_vals,gamma_inv=gamma_inv,lwd=5)
+)
 
 
 # Part c
 tmax <- 40 # end time for numerical integration of the ODE
-## draw box for plot:
-plot(0,0,xlim=c(0,1/gamma_inv*tmax),ylim=c(0,0.5),
-     type="n",xlab="Time (t)",ylab="Prevalence (I)",las=1)
 ## initial conditions:
 I0 <- 0.001
 S0 <- 1 - I0
 R0 <-  0
 gamma_inv=4
-## draw solutions for several values of parameter beta:
 Rknot_vals <- c(1.2,1.5,1.8,2,3,4)
+## draw box for plot:
+plot(0,0,xlim=c(0,tmax),ylim=c(0,0.5),
+     type="n",xlab="Time (t) in days",ylab="Prevalence (I)",las=1)
+## draw solution for each value of Rknot:
 for (i in 1:length(Rknot_vals)) {
   draw.soln(ic=c(S=S0,I=I0,R=R0), tmax=tmax,
-            times=1/gamma_inv*seq(0,tmax,length.out = 500),
+            times=seq(0,tmax,length.out = 500),
             func=SIR.vector.field,
-            parms=c(R_0=Rknot_vals[i],gamma=1), lwd=2,
+            parms=c(R_0=Rknot_vals[i],gamma_inv=gamma_inv), lwd=2,
             colour=rainbow(length(Rknot_vals)+1)[i] # use a different line colour for each solution
   )
 }
 legend("topright",legend=Rknot_vals,col=rainbow(length(Rknot_vals)+1),lwd=2)
 
 
+## Part d
+tmax <- 122 # end time for numerical integration of the ODE
+euc.dist <- function(x1, x2) sqrt(sum((x1 - x2) ^ 2))
+Errors <- matrix(NA,ncol=4,nrow=4*6*6)
+y1 <- data.frame(time=philadata$t,y=philadata$pim)
+pop=c(900,2000,2500,3000,5000)
+for (p in 1:length(pop)) {
+  ## initial conditions:
+  I0 <- 0.00002
+  S0 <- 1 - I0
+  R0 <-  0.1
+  plot(philadata$t,philadata$pim/5100,pch=20,col="blue",cex=1.2)
+  gamma_inv <- 4
+  Rknot_vals <- c(1.97,2,2.01,2.03,2.05)
+  ## draw solution for each value of Rknot:
+  for (i in 1:length(Rknot_vals)) {
+    for (j in 1:length(gamma_inv)) {
+      y2 <- draw.soln(ic=c(S=S0,I=I0,R=R0), tmax=tmax,
+                times=seq(0,tmax,length.out = 488+1),
+                func=SIR.vector.field,
+                parms=c(R_0=Rknot_vals[i],gamma_inv=gamma_inv[j]), lwd=2, lty=j,
+                colour=rainbow(length(Rknot_vals)+1)[i] # use a different line colour for each solution
+      )
+      func <- y2[which(y2$time %in% y1$time),"y"]
+      E <- euc.dist(y1[,2],func)
+      Errors[((p-1)*length(Rknot_vals)*length(gamma_inv)+(i-1)*length(gamma_inv)+j),1:4] <- c(pop[p],Rknot_vals[i],gamma_inv[j],E)
+    }
+  }
+  
+  legend("topright",legend=Rknot_vals,col=rainbow(length(Rknot_vals)+1),lwd=2)
+  title(paste("Population of",pop[p]))
+}
+
+plot(NULL)
